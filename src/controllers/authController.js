3611f57f2ba9a10b403e07ee4bda0bac6fd4db1c
CarlaPebaciniORT25 <carlacelestepebacini@gmail.com>
2026-06-17 12:38:15 -0300

    Agregue RefreshToken

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,21 +27,20 @@ const login = async (req, res) => {
       return res.status(401).json({    error: " usuario o contraseña incorrecta"  });
     }
 
-    const token = jwt.sign(
-      {
-        id: usuario._id,
-        email: usuario.email,
-        rol: usuario.rol,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "1d",
-      },
-    );
+
+     const token = generarToken(usuario) 
+     const refreshToken = generarRefresh(usuario)
+
+     res.cookie("refreshToken" , refreshToken ,{
+        httpOnly : true,
+        maxAge: 100*60*60*24*7 // 7 dias en miliseg
+     })
+
+
 
     res.status(200).json({
       mensaje: "Login exitoso",
-      token,
+      token
     });
 
   } catch (error) {
@@ -53,6 +52,63 @@ const login = async (req, res) => {
     });
   }
 };
+
+const generarToken = (usuario) =>{
+  const datosEncriptados = 
+      {
+        id: usuario._id,
+        email: usuario.email,
+        rol: usuario.rol
+      }
+
+   const JWY_KEY =   process.env.JWT_SECRET
+
+     return jwt.sign(
+  datosEncriptados,
+   JWY_KEY,
+   {  expiresIn: "1d"  }
+)
+
+}
+
+
+const generarRefresh =(usuario) => {
+
+
+  return jwt.sign(
+    {id: usuario._id},
+    process.env.JWT_SECRET,
+    {expiresIn: "7d"}
+  )
+}
+
+const refreshToken = (req , res) =>{
+const tokenn = req.cookies.refreshToken
+
+if(!tokenn){
+  return res.status(401).json ({ error: " No hay refresh token"})
+}
+
+try{
+
+  const decodificado = jwt.verify(tokenn , process.env.JWT_SECRET_REFRESH)
+
+  const nuevoAccesToken = jwt.sign(
+  { id:decodificado.id},
+    process.env.JWT_SECRET,
+    { expiresIn: '1h'}
+  )
+
+  res.json({token: nuevoAccesToken})
+
+}catch(error){
+res.status(500).json({  error: "hubo un error" });
+}
+
+}
+
+
+
 
 const perfil = async (req, res) => {
 
